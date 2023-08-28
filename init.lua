7b05945e61d63c8851b92663ce8f9be84069bc87
Žan Fridau <zaanfr@gmail.com>
2023-08-28 14:11:24 +0200

    Small fix to toggleterm

--- init.lua
+++ init.lua
@@ -1,42 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -217,8 +178,7 @@ require('lazy').setup({
 
   -- MY CUSTOM PLUGINS
 
-
-  { 'akinsho/toggleterm.nvim', version = "*", opts = { --[[ things you want to change go here]] } },
+  { 'akinsho/toggleterm.nvim', version = "*", config = true },
 
   {
     'numToStr/Comment.nvim',
@@ -587,6 +547,10 @@ vim.keymap.set('n', '<leader>sr', ':%s//', { desc = 'Start search and replace' }
 vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = 'opens lazygit' })
 vim.keymap.set({ 'n', 'x', 'i' }, '<C-s>', '<esc>:w<CR>', { desc = 'formats and saves' })
 vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'toggles neotree' })
-vim.keymap.set('n', '<leader>tv', ':ToggleTerm<CR>', { desc = 'toggles vertical terminal' })
-vim.keymap.set('n', '<leader>tt', ':ToggleTerm direction=float<CR>', { desc = 'toggles floating terminal' })
+vim.keymap.set('n', '<leader>tt', ':ToggleTerm<CR>', { desc = 'toggles vertical terminal' })
+vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<CR>', { desc = 'toggles floating terminal' })
 vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], { noremap = true, })
+vim.keymap.set('n', '<leader>bj', '<C-w>k', { desc = 'Move to window above' })
+vim.keymap.set('n', '<leader>bk', '<C-w>j', { desc = 'Move to window below' })
+vim.keymap.set('n', '<leader>bh', '<C-w>h', { desc = 'Move to window to the left' })
+vim.keymap.set('n', '<leader>bl', '<C-w>l', { desc = 'Move to window to the right' })
