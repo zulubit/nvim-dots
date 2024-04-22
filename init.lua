4328052d2e6e17a42eed748aa554f5e27c09bf1b
Žan Fridau <zaanfr@gmail.com>
2024-04-22 10:56:02 +0200

    tabstop 2

--- init.lua
+++ init.lua
@@ -87,7 +87,7 @@ p.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-
+vim.opt.tabstop = 4
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
