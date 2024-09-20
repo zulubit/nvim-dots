adf8c6b7b18dffcdfa7641921f11580a4db2914d
Žan Fridau <zaanfr@gmail.com>
2024-09-20 13:42:48 +0200

    fixed flash

--- init.lua
+++ init.lua
@@ -943,12 +943,8 @@ require('lazy').setup({
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    ---@type Flash.Config
     opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  },
+    -- stylua: ignore
   },
 
   {
